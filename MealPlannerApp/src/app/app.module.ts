7495156994d6deeb0dc9684cf49c498f33b55b75
Nicolas Seay <nicolas.seay@gmail.com>
2022-12-24 23:45:36 -0500

    Minor position changes. Added login ngModels

--- MealPlannerApp/src/app/app.module.ts
+++ MealPlannerApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,7 +15,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MealPlannerApp/src/app/login/login.component.ts
+++ MealPlannerApp/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string = ''
+  password: String = ''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.username, this.password)
   }
 
 }
