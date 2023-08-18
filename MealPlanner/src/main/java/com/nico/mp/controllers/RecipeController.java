ddb400ed270ec7ba3c08a51ac2ac4d224d324dff
Nicolas Seay <nicolas.seay@gmail.com>
2023-08-18 09:22:28 -0400

    Cleaned up RecipeController

--- MealPlanner/src/main/java/com/nico/mp/controllers/RecipeController.java
+++ MealPlanner/src/main/java/com/nico/mp/controllers/RecipeController.java
@@ -33,49 +33,28 @@ public class RecipeController {
 			: new ResponseEntity<>(recipes, HttpStatus.OK);
 	}
 	
-//	@PostMapping("/add")
-//	public ResponseEntity<Void> addRecipe(
-//			@RequestBody Recipe recipe,
-//			@RequestHeader(required = false) String authorization
-//	) {
-//		log.info("AddRecipe - received addRecipe request");
-//		if (authorization == null || !jwtUtil.validateTokenOld(recipe.getUserId(), authorization.substring(7))) {
-//			log.info("AddRecipe - JWT authentication failed");
-//			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//		}
-//		log.info("AddRecipe - JWT authentication succeeded");
-//
-//		try {
-//			recipeService.saveRecipe(recipe);
-//		} catch (Exception e) {
-//			log.info("AddRecipe - recipe not added/updated", e);
-//			return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-//		}
-//		log.debug("AddRecipe - recipe added/updated");
-//		return new ResponseEntity<>(HttpStatus.OK);
-//	}
-//
-//	@DeleteMapping("/remove/{userId}/recipe/{recipeId}")
-//	public ResponseEntity<Void> deleteRecipe(
-//			@PathVariable long userId,
-//			@PathVariable long recipeId,
-//			@RequestHeader(required = false) String authorization
-//	) {
-//		log.info("DeleteRecipe - received deleteRecipe request");
-//		if (authorization == null || !jwtUtil.validateTokenOld(userId, authorization.substring(7))) {
-//			log.info("DeleteRecipe - JWT authentication failed");
-//			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//		}
-//		log.info("DeleteRecipe - JWT authentication succeeded");
-//
-//		try {
-//			recipeService.deleteRecipe(recipeId);
-//		} catch (Exception e) {
-//			log.info("DeleteRecipe - recipe not deleted", e);
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		}
-//		log.debug("DeleteRecipe - recipe deleted");
-//		return new ResponseEntity<>(HttpStatus.OK);
-//	}
+	@PostMapping("/add")
+	public ResponseEntity<Void> addRecipe(@RequestBody Recipe recipe) {
+		try {
+			recipeService.saveRecipe(recipe);
+		} catch (Exception e) {
+			log.info("AddRecipe - recipe not added/updated", e);
+			return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+		}
+		log.debug("AddRecipe - recipe added/updated");
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@DeleteMapping("/remove/{userId}/recipe/{recipeId}")
+	public ResponseEntity<Void> deleteRecipe(@PathVariable long userId, @PathVariable long recipeId) {
+		try {
+			recipeService.deleteRecipe(recipeId);
+		} catch (Exception e) {
+			log.info("DeleteRecipe - recipe not deleted", e);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		log.debug("DeleteRecipe - recipe deleted");
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
