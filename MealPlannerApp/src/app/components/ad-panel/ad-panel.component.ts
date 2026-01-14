a945f05144e64d851fbc1a0dacec3e303aa3be4e
Nicolas Seay <nicolas.seay@gmail.com>
2026-01-13 23:37:16 -0500

    Fixes to accomodate angular upgrades

--- MealPlannerApp/src/app/components/ad-panel/ad-panel.component.ts
+++ MealPlannerApp/src/app/components/ad-panel/ad-panel.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ad-panel',
   templateUrl: './ad-panel.component.html',
-  styleUrls: ['./ad-panel.component.scss']
+  styleUrls: ['./ad-panel.component.scss'],
+  standalone: false
 })
 export class AdPanelComponent {
 
